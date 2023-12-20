1b2b94e0bedf88bf4cc93985943540bdf6d2b080
ganesh <mallelaganesh990@gmail.com>
2023-12-20 15:56:31 +0530

    first commit

---
+++ rytetye.py
@@ -0,0 +1,5 @@
+a=input('enter the character:-')
+if chr(a)==(a,e,i,o,u):
+    print('it is a vowel')
+else:
+    print('it is not vowel')
---
+++ vowel.py
@@ -0,0 +1,5 @@
+a=input('enter a character :-')
+if a>='A'<=a <='z':
+    print('it is an uppercase')
+    else:
+        print('not an upper')
